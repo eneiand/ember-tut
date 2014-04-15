86a7f55cdf529060afad282f281c5d22e8419fe7
Neil Anderson <neil.t.anderson@gmail.com>
2014-04-15 22:45:09 +0100

    adding the TodosRoute since we need custom behaviour.

--- js/router.js
+++ js/router.js
@@ -2,3 +2,9 @@ Todos.Router.map(function () {
   //detect the '/' URL and render the todos template
   this.resource('todos', { path: '/' });
 });
+
+Todos.TodosRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find('todo');
+  }
+});
