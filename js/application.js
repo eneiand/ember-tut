5835045000bce18fe8e0e8e7ff61f311d838cf08
Neil Anderson <neil.t.anderson@gmail.com>
2014-04-15 22:09:24 +0100

    instantiate an Ember.Application, add a route, wrap the inner body tag in a handlebars script.

---
+++ js/application.js
@@ -0,0 +1,3 @@
+//create an instance of Ember.Application and make it available as Todos 
+//variable
+window.Todos = Ember.Application.create();
---
+++ js/router.js
@@ -0,0 +1,4 @@
+Todos.Router.map(function () {
+  //detect the '/' URL and render the todos template
+  this.resource('todos', { path: '/' });
+});
