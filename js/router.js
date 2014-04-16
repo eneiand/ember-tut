8d78d7c0953cdb8442ec23ba3ee247e6179e26c6
Neil Anderson <neil.t.anderson@gmail.com>
2014-04-16 15:34:01 +0100

    move list into its own template, add rout for TodosActive

--- js/router.js
+++ js/router.js
@@ -1,11 +1,30 @@
 Todos.Router.map(function () {
   //detect the '/' URL and render the todos template
-  this.resource('todos', { path: '/' });
+  this.resource('todos', { path: '/' }, function() {
+    this.route('active');
+  });
 });
 
 //class with a model function that returns all existing todos
 Todos.TodosRoute = Ember.Route.extend({
   model: function() {
     return this.store.find('todo');
+  }
+});
+
+Todos.TodosIndexRoute = Ember.Route.extend({
+  model: function() {
+    return this.modelFor('todos');
+  }
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+  model: function(){
+    return this.store.filter('todo', function(todo) {
+      return !todo.get('isCompleted');
+    });
+  },
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller});
   }
 });
