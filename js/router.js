f59e450431b6ea2914ee420fcca35a31c451683e
Neil Anderson <neil.t.anderson@gmail.com>
2014-04-16 15:37:14 +0100

    same child route logic for the completed filter

--- js/router.js
+++ js/router.js
@@ -1,7 +1,9 @@
 Todos.Router.map(function () {
   //detect the '/' URL and render the todos template
   this.resource('todos', { path: '/' }, function() {
+    //child routes
     this.route('active');
+    this.route('completed');
   });
 });
 
@@ -22,6 +24,18 @@ Todos.TodosActiveRoute = Ember.Route.extend({
   model: function(){
     return this.store.filter('todo', function(todo) {
       return !todo.get('isCompleted');
+    });
+  },
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller});
+  }
+});
+
+
+Todos.TodosCompletedRoute = Ember.Route.extend({
+  model: function(){
+    return this.store.filter('todo', function(todo) {
+      return todo.get('isCompleted');
     });
   },
   renderTemplate: function(controller) {
