92a98ff55f3b2ccdbcfe9b511e4b708f71904245
Neil Anderson <neil.t.anderson@gmail.com>
2014-04-16 13:35:49 +0100

    fix the number of completed todos

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -1,17 +1,27 @@
 Todos.TodosController = Ember.ArrayController.extend({
   actions: {
     createTodo: function() {
-      var title = this.get('newTitle');
-      if(!title.trim()) { return; }
+        var title = this.get('newTitle');
+        if(!title.trim()) { return; }
+
+        var todo = this.store.createRecord('todo', {
+          title: title,
+          isCompleted: false
+        });
       
-      var todo = this.store.createRecord('todo', {
-        title: title,
-        isCompleted: false
-      });
+        this.set('newTitle', '');
       
-      this.set('newTitle', '');
-      
-      todo.save();
-    }
-  }
+        todo.save();
+      }
+    },
+    
+    remaining: function() {
+      return this.filterBy('isCompleted', false).get('length');
+    }.property('@each.isCompleted'),
+    
+    inflection: function() {
+      var remaining = this.get('remaining');
+      return remaining === 1 ? 'item' : 'items';
+    }.property('remaining')
+    
 });
